002be7900439f9cfaf213d48031ca26c7f453b4b
Kacton <kacton113@gmail.com>
2023-07-19 17:28:51 -0700

    got the models, urls, and views added

--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+from common.json import ModelEncoder
+from .models import Shoe
+
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "size",
+        "color",
+        "brand",
+    ]
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request):
+    if request.method == "GET":
+        shoes = Shoe.objects.all()
+        return JsonResponse(
+            {"shoes":shoes},
+            encoder=ShoeListEncoder,
+            safe=False
+        )
+    else:
+        content = json.loads(request.body)
+        shoe = Shoe.objects.create(**content)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeListEncoder,
+            safe=False
+        )
+
+def api_show_shoe(request, pk):
+    shoe = Shoe.objects.get(id=pk)
+    return JsonResponse(
+        shoe,
+        encoder=ShoeListEncoder,
+        safe=False
+    )
