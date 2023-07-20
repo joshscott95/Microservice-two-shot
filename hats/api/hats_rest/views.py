7e17fb164286b879dfa130e1a868f4bf52b38517
joshscott95 <joshscott95@yahoo.com>
2023-07-19 17:41:20 -0700

    successfully implemented a Hats model and its correlating view functions. Urls and settings updated. Can make PUT and GET requests in insomniafor all hats or for a specific hat

--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,80 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+from common.json import ModelEncoder
+from .models import Hat
+
+
+class HatEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "id",
+        "fabric",
+        "style_name",
+        "color",
+        "picture_url",
+    ]
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_hats(request):
+    if request.method == "GET":
+        hats = Hat.objects.all()
+        return JsonResponse(
+            {"hats": hats},
+            encoder=HatEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        hat = Hat.objects.create(**content)
+        return JsonResponse(
+            hat,
+            encoder=HatEncoder,
+            safe=False,
+        )
+
+@require_http_methods(["DELETE", "GET", "PUT"])
+def api_list_hat(request, pk):
+    if request.method == "GET":
+        try:
+            hat = Hat.objects.get(id=pk)
+            return JsonResponse(
+                hat,
+                encoder=HatEncoder,
+                safe=False
+            )
+        except Hat.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
+    elif request.method == "DELETE":
+        try:
+            hat = Hat.objects.get(id=pk)
+            hat.delete()
+            return JsonResponse(
+                hat,
+                encoder=HatEncoder,
+                safe=False,
+            )
+        except Hat.DoesNotExist:
+            return JsonResponse({"message": "Does not exist"})
+    else: # PUT
+        try:
+            content = json.loads(request.body)
+            hat = Hat.objects.get(id=pk)
+
+            props = ["fabric", "style_name", "color", "picture_url", "wardrobe_location"]
+            for prop in props:
+                if prop in content:
+                    setattr(hat, prop, content[prop])
+            hat.save()
+            return JsonResponse(
+                hat,
+                encoder=HatEncoder,
+                safe=False,
+            )
+        except Hat.DoesNotExist:
+            response = JsonResponse({"message": "Does not exist"})
+            response.status_code = 404
+            return response
